43f0d98ba33ade3228cf56f89b05241ef2744ffb
Tom Markoc (Aditi) <REDMOND\a-tomark>
2013-08-27 18:58:55 -0700

    parent waits for zombie to be created before countdown

--- source/code/scxsystemlib/process/tools/zombie.cpp
+++ source/code/scxsystemlib/process/tools/zombie.cpp
@@ -1,14 +1,20 @@
 // To compile:
 // Solaris:
 // CC zombie.cpp -o zombie
-// Linux:
+// Linux (2.6.9 and newer kernels):
 // g++ zombie.cpp -o zombie
+// Linux (kernels older than 2.6.9):
+// g++ zombie.cpp -Dold_linux -o zombie
 // AIX:
 // xlC zombie.cpp -o zombie
 // HPUX:
 // aCC zombie.cpp -o zombie
 #include<unistd.h>
 #include<stdio.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<sstream>
+#include<fstream>
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -30,12 +36,73 @@ int main(int argc, char *argv[])
     pid_t pid = fork();
     if (pid == -1)
     {
+        printf("fork() failed!\n");
         return -1;
     }
     if (pid == 0)
     {
+//        sleep(5);
         return 0;
     }
+#if !defined(old_linux)
+    siginfo_t info;
+    memset(&info, 0, sizeof(info));
+    if (waitid(P_PID, pid, &info, WEXITED|WNOWAIT) != 0)
+    {
+        printf("waitid() failed!\n");
+        return -1;
+    }
+    // Call for the second time just to verify zombie is still arround after the first call.
+    memset(&info, 0, sizeof(info));
+    if (waitid(P_PID, pid, &info, WEXITED|WNOWAIT) != 0)
+    {
+        printf("waitid() failed!\n");
+        return -1;
+    }
+    if (info.si_code == CLD_EXITED)
+    {
+        printf("Zombie created!\n");
+    }
+    else
+    {
+        printf("Failed to create zombie!\n");
+        return -1;
+    }
+#else
+    std::stringstream pidStr;
+    pidStr << "/proc/" << pid << "/stat";
+    unsigned int timeout = 10;
+    unsigned int t;
+    for (t = 0; t < timeout; t++)
+    {
+        std::ifstream proc(pidStr.str().c_str());
+        pid_t procPid = 0;
+        std::string procName;
+        char procState;
+        proc >> procPid >> procName >> procState;
+        if (proc.good() != true)
+        {
+            printf("Failed to create zombie - error in zombie process file!\n");
+            return -1;
+        }
+        if (procPid != pid)
+        {
+            printf("Failed to create zombie - error in zombie process file - pid doesn't match!\n");
+            return -1;
+        }
+        if (procState == 'Z')
+        {
+            printf("Zombie created!\n");
+            break;
+        }
+        sleep(1);
+    }
+    if (t >= timeout)
+    {
+        printf("Timed out waiting for the zombie!\n");
+        return -1;
+    }
+#endif
     while (lifetime > 0)
     {
         printf(" zombie lives %i more seconds\n", lifetime);
