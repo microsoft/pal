c498940617edecab52f70b82a933a442d86718b0
Jeff Coffler <jeff@taltos.com>
2016-11-21 14:41:20 -0800

    Allow PPC builds to be identified as REDHAT, not ULINUX
    These builds only run on PPC platform (Power*) hosted by AIX

--- installer/InstallBuilder/unittests/ib_unittests.py
+++ installer/InstallBuilder/unittests/ib_unittests.py
@@ -252,6 +252,8 @@ if Variables["PF"] == "Linux":
         PACKAGE_TYPE = "RPM"
     elif (Variables["PFDISTRO"] == "ULINUX" and Variables["PF_DISTRO_ULINUX_KIT"] == "D"):
         PACKAGE_TYPE = "DPKG"
+    elif (Variables["PFDISTRO"] == "REDHAT" and Variables["PFARCH"] == "ppc"):
+        PACKAGE_TYPE = "RPM"
     else:
         error("Invalid Platform")
 elif Variables["PF"] == "AIX":
